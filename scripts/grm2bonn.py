6ad75a713753c2d8ac2c16a93655972c586c9de4
Gabriel-Reis <g.requenareis@gmail.com>
2020-07-03 13:54:02 -0300

    Update

--- scripts/grm2bonn.py
+++ scripts/grm2bonn.py
@@ -3,15 +3,12 @@
 
 # ## grm to bonn moviments
 
-# In[61]:
-
-
 import pandas as pd
 import os
 import gzip
 
-
 # In[64]:
+print("Starting...")
 fileName = "trace"
 BonnMotionPath = "../bonnmotion-3.0.1/bin/"
 
@@ -21,17 +18,18 @@ data = pd.read_csv("../traces/"+fileName+".csv", names =  ['time', 'id', 'x', 'y
 # Converting to cooja format
 data = data[['id', 'time', 'x', 'y']]
 
+# Sorting by time and exporting to csv.
+data.sort(['id','time']).to_csv("../traces/"+fileName+"-cooja.csv", sep = ' ', index=False, header=False )
+
 #removing erros
 data = data.drop_duplicates(['id', 'time']) # same node in more than one position.
-
-# Sorting by time and exporting to csv.
-data.sort('time').to_csv("../traces/"+fileName+"-cooja.csv", sep = ' ', index=False, header=False )
 
 
 input_file = "../traces/"+fileName+"-cooja.csv"
 output_file = os.path.basename(input_file)[:-3] + "movements"
 node_info = {}
 
+print("1/5 DONE")
 count = 0
 # parse cooja trace 2 bonn movements
 with open(input_file, "r") as infile:
@@ -44,17 +42,13 @@ with open(input_file, "r") as infile:
         else:
             node_info[int(l[0])] = []
             node_info[int(l[0])].append(l[1]+" "+l[2]+" "+l[3]+" ")
-        
-        """print(l)
-        if count == 10:
-            break
-        else:
-            count += 1"""
+print("2/5 DONE")
 
-# Writing movements
+# # Writing movements
 with open("../traces/" + output_file, "w") as outfile:
     for node in sorted(node_info.keys()):
         outfile.write(''.join(str(e) for e in node_info[node])+"\n")
+print("3/5 DONE")
 
 # compacting movements
 with open("../traces/" + output_file, 'rb') as in_f:
@@ -62,9 +56,10 @@ with open("../traces/" + output_file, 'rb') as in_f:
     bindata = bytearray(data)
     with gzip.open(BonnMotionPath + output_file + ".gz", "wb") as out_f:
         out_f.write(bindata)
+print("4/5 DONE")
 
 # Writing .stats 
-with open("../traces/" + fileName + ".params", "w") as outfile:
+with open(BonnMotionPath+fileName + "-cooja" + ".params", "w") as outfile:
     outfile.write("model=SWIM"+"\n")
     outfile.write("ignore=3600.0"+"\n")
     outfile.write("randomSeed=1592456788185"+"\n")
@@ -75,7 +70,5 @@ with open("../traces/" + fileName + ".params", "w") as outfile:
     outfile.write("circular=false"+"\n")
     outfile.write("J=2D"+"\n")
     outfile.write("nodeRadius=0.1")
-# cellDistanceWeight=0.0
-# nodeSpeedMultiplier=1.0
-# waitingTimeExponent=0.0
-# waitingTimeUpperBound=0.0
+print("5/5 DONE")
+print("END")
